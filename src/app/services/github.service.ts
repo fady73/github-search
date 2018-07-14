fb6330cb433c412abe98c3eb26b4de99156046b5
fady khayrat <fadykhayrat@gmail.com>
2018-07-14 19:33:23 +0200

    Update github.service.ts

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -4,9 +4,9 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService{
-    private username = 'bradtraversy';
-    private client_id = 'd9308aacf8b204d361fd';
-    private client_secret='62551cc02cee983fff0bac41baf170eb5a312c1c';
+    private username = 'fady73';
+    private client_id = 'e082346e45189428be25';
+    private client_secret='7c296ee173390dc0cceb0a38a6151f8fea771e4b';
     
     constructor(private _http:Http){
         console.log('Github Service Init...');
